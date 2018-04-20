b0448ef7265878c54a479198220a2bc658a73c2b
John Crisostomo <johncrisostomo@protonmail.ch>
2018-04-20 17:53:05 +0800

    Initial commit

---
+++ contracts/Lottery.sol
@@ -0,0 +1,33 @@
+contract Lottery {
+    address public manager;
+    address[] public players;
+    
+    constructor() public {
+        manager = msg.sender;
+    }
+    
+    modifier restrictedToManager() {
+        require(msg.sender == manager);
+        _;
+    }
+    
+    function enter() public payable {
+        require(msg.value > .01 ether);
+        players.push(msg.sender);
+    }
+    
+    function random() private view returns (uint){
+        return uint(keccak256(block.difficulty, now, players));
+    }
+    
+    function pickWinnder() public restrictedToManager {
+        uint index = random() % players.length;
+        players[index].transfer(address(this).balance);
+        
+        players = new address[](0);
+    }
+    
+    function getPlayers() public view returns (address[]) {
+        return players;
+    }
+}
---
+++ deploy.js
@@ -0,0 +1,31 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const { accountMnemonic, infuraApiKey } = require('./settings.json');
+
+const network = 'rinkeby';
+const INFURA_API_URI = `https://${network}.infura.io/${infuraApiKey}`
+
+const provider = new HDWalletProvider(
+    accountMnemonic,
+    INFURA_API_URI
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    console.log(`Fetched accounts: ${accounts}`);
+
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode })
+        .send({ gas: '1000000', from: accounts[0] });
+
+    result.setProvider(provider); 
+
+    console.log(`Contract address is ${result.options.address}`);
+};
+
+deploy();

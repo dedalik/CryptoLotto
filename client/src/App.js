a88766c0ca4c8653ae0d0a5a0c40b12a8cebffac
John Crisostomo <johncrisostomo@protonmail.ch>
2018-04-21 16:28:26 +0800

    Implements pickWinner

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,8 @@ class App extends Component {
         manager: '',
         balance: '',
         players: [],
-        value: ''
+        value: '',
+        message: ''
     };
 
     async componentDidMount() {
@@ -23,25 +24,45 @@ class App extends Component {
 
         const accounts = await web3.eth.getAccounts();
 
+        this.setState({ message: 'Waiting for transaction to complete. . .' });
+
         await lottery.methods.enter().send({
             value: web3.utils.toWei(this.state.value, 'ether'),
             from: accounts[0]
         });
+
+        this.setState({ message: 'Done!' });
     };
 
+    handlePickWinner = async () => {
+        const accounts = await web3.eth.getAccounts();
+
+        this.setState({ message: 'Waiting for transaction to complete.' });
+
+        await lottery.methods.pickWinner().send({
+            from: accounts[0]
+        });
+
+        this.setState({ message: 'Done!' });
+    };
+
+    renderPickWinner = () => (
+        <div>
+            <button onClick={this.handlePickWinner}>Pick a Winner!</button>
+        </div>
+    );
+
     render() {
+        const { manager, players, balance, value, message } = this.state;
+
         return (
             <div>
                 <h2>CryptoLotto!</h2>
                 <p>
-                    Game is currently managed by{' '}
-                    <code>{this.state.manager}</code>
+                    Game is currently managed by <code>{manager}</code>
                 </p>
-                <p>Players: {this.state.players.length}</p>
-                <p>
-                    Jackpot: {web3.utils.fromWei(this.state.balance, 'ether')}{' '}
-                    ether
-                </p>
+                <p>Players: {players.length}</p>
+                <p>Jackpot: {web3.utils.fromWei(balance, 'ether')} ether</p>
 
                 <hr />
 
@@ -50,7 +71,7 @@ class App extends Component {
                     <div>
                         <label>Amount of ether : </label>
                         <input
-                            value={this.state.value}
+                            value={value}
                             onChange={event =>
                                 this.setState({ value: event.target.value })
                             }
@@ -58,6 +79,16 @@ class App extends Component {
                     </div>
                     <button>Enter</button>
                 </form>
+
+                <hr />
+
+                <h5>{message}</h5>
+
+                {/* Just hardcoding this here since this is just a crude sample app*/}
+                {manager &&
+                manager === '0x1EAc978c8A9DC1d0Ab999bB1DdfF078477F8Af16'
+                    ? this.renderPickWinner()
+                    : null}
             </div>
         );
     }
